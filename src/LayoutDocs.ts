fe98add250856085e81cfda98a67136ddcb85317
CD Cabrera <ccabrera@redhat.com>
2025-09-12 14:33:34 -0400

    fix: github resource path

--- src/LayoutDocs.ts
+++ src/LayoutDocs.ts
@@ -1,13 +1,13 @@
 import { DesignGuidelinesDir } from './const.js';
 
-export const LayoutsDir = 'layout';
+export const LayoutsDir = 'layouts';
 
 export const LayoutDocs = [
-  `[@patternfly/Bullseye - Design Guidelines](${DesignGuidelinesDir}/${LayoutsDir}/about-modal/bullseye.md)`,
-  `[@patternfly/Flex - Design Guidelines](${DesignGuidelinesDir}/${LayoutsDir}/about-modal/flex.md)`,
-  `[@patternfly/Gallery - Design Guidelines](${DesignGuidelinesDir}/${LayoutsDir}/about-modal/gallery.md)`,
-  `[@patternfly/Grid - Design Guidelines](${DesignGuidelinesDir}/${LayoutsDir}/about-modal/grid.md)`,
-  `[@patternfly/Level - Design Guidelines](${DesignGuidelinesDir}/${LayoutsDir}/about-modal/level.md)`,
-  `[@patternfly/Split - Design Guidelines](${DesignGuidelinesDir}/${LayoutsDir}/about-modal/split.md)`,
-  `[@patternfly/Stack - Design Guidelines](${DesignGuidelinesDir}/${LayoutsDir}/about-modal/stack.md)`,
+  `[@patternfly/Bullseye - Design Guidelines](${DesignGuidelinesDir}/${LayoutsDir}/bullseye.md)`,
+  `[@patternfly/Flex - Design Guidelines](${DesignGuidelinesDir}/${LayoutsDir}/flex.md)`,
+  `[@patternfly/Gallery - Design Guidelines](${DesignGuidelinesDir}/${LayoutsDir}/gallery.md)`,
+  `[@patternfly/Grid - Design Guidelines](${DesignGuidelinesDir}/${LayoutsDir}/grid.md)`,
+  `[@patternfly/Level - Design Guidelines](${DesignGuidelinesDir}/${LayoutsDir}/level.md)`,
+  `[@patternfly/Split - Design Guidelines](${DesignGuidelinesDir}/${LayoutsDir}/split.md)`,
+  `[@patternfly/Stack - Design Guidelines](${DesignGuidelinesDir}/${LayoutsDir}/stack.md)`,
 ];
